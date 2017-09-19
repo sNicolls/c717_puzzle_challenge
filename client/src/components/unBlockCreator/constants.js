97213d0bffaae9f8db3c866599adb98c7d88db21
ShaneN <scnicolls@charter.net>
2017-09-19 00:38:35 -0700

    getting there

--- client/src/components/unBlockCreator/constants.js
+++ client/src/components/unBlockCreator/constants.js
@@ -2,9 +2,14 @@ export const pieceStack = [];
 
 
 export const playableStack = [
-    {type: "unBlock_longPiece", axis: "x", xPos: "2/4", yPos: "0/0"},
-    {type: "unBlock_tallPiece", axis: "y", xPos: "3", yPos: "2/4"},
+    {type: "unBlock_tallPiece", axis: "y", xPos: 3, yPos: 2},
+
     ];
 
 
+
+export const pieceCoord = [];
+
+
+export const boardWidth = null;
 
--- client/src/components/unBlockCreator/gameBoard.js
+++ client/src/components/unBlockCreator/gameBoard.js
@@ -5,36 +5,155 @@ export default class extends Component {
     constructor(props){
         super(props);
         this.handleDrag = this.handleDrag.bind(this);
+        this.handleEnd = this.handleEnd.bind(this);
+        this.state = {
+            gameBoardWidth: null,
+            oneBoardUnit: null,
+
+            tallPieceWidth: null,
+            tallPieceHeight: null,
+            longPieceHeight: null,
+            longPieceWidth: null,
+
+            piecePos: null,
+            holderX: 0,
+            mainX: 0,
+            mainY: 0,
+            needsReset: false,
+            posToResetTo: 0,
+        }
+    }
+    //WHEN THE COMPONENT MOUNTS, SET BOARD WIDTH
+    componentDidMount(){
+        const gameBoardWidth = document.getElementsByClassName("gameBoardDiv")[0].clientHeight;
+        const oneBoardUnit = document.getElementsByClassName("gameBoardDiv")[0].clientHeight/6;
+
+        this.setState({
+            gameBoardWidth: gameBoardWidth,
+            oneBoardUnit: oneBoardUnit,
+            mainY: oneBoardUnit*2,
+            tallPieceHeight: oneBoardUnit*2,
+            tallPieceWidth: oneBoardUnit,
+            longPieceHeight: oneBoardUnit,
+            longPieceWidth: oneBoardUnit*2
+
+        })
+
     }
 
     handleDrag(ev){
-        console.log(ev.target.getBoundingClientRect())
-        //console.log(document.getElementsByClassName("unBlock_startingPiece")[0].getBoundingClientRect());
-        if(document.getElementsByClassName("unBlock_startingPiece")["0"].style.transform.match(/\d+/g)[0] === "205"){
-            console.log("WIN")
+
+        var thisPiecePos = ev.target.getBoundingClientRect();
+        var thisPieceIndex = ev.target.id;
+
+        var pieces = document.getElementsByClassName("gamePiece");
+        var starterPiecePos = document.getElementsByClassName("starterPiece")[0].getBoundingClientRect();
+        var piecePosArr = [];
+
+
+        for (var i = 0; i < pieces.length; i++) {
+            piecePosArr.push(pieces[i].getBoundingClientRect())
         }
 
+
+        piecePosArr.map((piecePos, index)=>{
+
+             if(index == thisPieceIndex){
+
+             } else if (thisPiecePos.right - piecePos.left > 1 && thisPiecePos.right - piecePos.left < -1){
+                 console.log(thisPiecePos.right)
+                 this.setState({
+                     holderX: thisPiecePos.right
+                 })
+            }
+        });
+
+
+        if(starterPiecePos.right + starterPiecePos.width > 408){
+            console.log("WIN")
+        }
+        //WIN CONDITION!!!!!!
+        // if(starterPiecePos.left){
+        //     console.log("WIN")
+        // }
+    }
+
+
+
+    handleEnd(ev){
+        console.log(ev.target.getBoundingClientRect().right)
+        this.setState({
+            mainX: this.state.holderX - ev.target.getBoundingClientRect().right
+        })
+
+        console.log(this.state.holderX)
     }
 render() {
     const pieceArr = [];
 
         this.props.pieceStack.map((piece, index)=>{
-            pieceArr.push(
-                <Draggable axis={piece.axis} bounds='parent' onDrag={this.handleDrag}>
-                    <div style={{gridColumn: piece.xPos, gridRow: piece.yPos, backgroundColor: "red", border: "solid", boxSizing: "border-box"}}></div>
-                </Draggable>)
-        })
+            const pieceType = piece.type
+            var pieceHeight = null;
 
+            if(piece.type === "unBlock_tallPiece"){
+                var yPos = this.state.oneBoardUnit*piece.yPos;
+                var xPos = this.state.oneBoardUnit*piece.xPos;
+
+                pieceArr.push(
+                    <Draggable axis={piece.axis} bounds='parent' onDrag={this.handleDrag}>
+                        <div id={index + 1} className={pieceType + ' gamePiece'} style={
+                            {
+                                width: this.state.tallPieceWidth,
+                                height: this.state.tallPieceHeight,
+                                top: yPos,
+                                left: xPos,
+                            }
+                        }
+                        >
+                        </div>
+                    </Draggable>
+                )
+            } else if(piece.type === "unBlock_longPiece"){
+                var yPos = this.state.oneBoardUnit*piece.yPos;
+                var xPos = this.state.oneBoardUnit*piece.xPos;
+
+                    pieceArr.push(
+                        <Draggable axis={piece.axis} bounds='parent' onDrag={this.handleDrag}>
+                            <div id={index + 1} className={pieceType + ' gamePiece'} style={
+                                {
+                                    width: this.state.longPieceWidth,
+                                    height: this.state.longPieceHeight,
+                                    top: yPos,
+                                    left: xPos,
+                                }
+                            }
+                            >
+                            </div>
+                        </Draggable>
+                    )
+            }
+
+        });
 
         return (
             <div className="gameBoardDiv">
-                <Draggable axis="x" bounds='parent' onDrag={this.handleDrag}>
-                    <div className="unBlock_startingPiece" style={{border: "solid", boxSizing: "border-box"}}></div>
+                <Draggable axis="x" bounds="parent"  onDrag={this.handleDrag} onStop={this.handleEnd}>
+                    <div
+                        id={0} className="starterPiece gamePiece" style={
+                            {
+                                top: this.state.oneBoardUnit*2,
+                                left: this.state.mainX,
+                                width: this.state.oneBoardUnit*2,
+                                height: this.state.oneBoardUnit
+                            }
+                        }
+                    >
+                    </div>
                 </Draggable>
                 {pieceArr}
             </div>
         )
     }
 }
-
-
+//position={{x: this.state.mainX, y: this.state.mainY}
+//${event.target.style.transform.match(/\d+/g)[0] - 1}
